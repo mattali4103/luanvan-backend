ddbf06b8fc83ac1cabb15391c6fcfa149970738d
Nguyễn Hải Lương <92219156+mattali4103@users.noreply.github.com>
2025-06-11 00:52:50 +0700

    Delete learning-progress directory

--- learning-progress/src/main/java/com/luanvan/learningprogress/Repository/HocPhanRepository.java
+++
@@ -1,7 +0,0 @@
-package com.luanvan.learningprogress.Repository;
-
-import com.luanvan.learningprogress.entity.HocPhan;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HocPhanRepository extends JpaRepository<HocPhan, Long> {
-}
--- learning-progress/src/main/java/com/luanvan/learningprogress/Repository/KetQuaHocTapRepository.java
+++
@@ -1,7 +0,0 @@
-package com.luanvan.learningprogress.Repository;
-
-import com.luanvan.learningprogress.entity.KetQuaHocTap;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface KetQuaHocTapRepository extends JpaRepository<KetQuaHocTap, Long> {
-}
