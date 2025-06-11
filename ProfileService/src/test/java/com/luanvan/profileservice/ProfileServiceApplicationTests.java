0f74fbd6a1d3b9a31ac5c779b5d4823667fc3830
mattalibodie <nguyenluongtt2@gmail.com>
2025-06-11 16:25:23 +0700

    add dockerfile

--- ProfileService/src/test/java/com/luanvan/profileservice/ProfileServiceApplicationTests.java
+++ ProfileService/src/test/java/com/luanvan/profileservice/ProfileServiceApplicationTests.java
@@ -1,18 +1,10 @@
 package com.luanvan.profileservice;
 
-import com.luanvan.profileservice.controller.SinhVienController;
-import com.luanvan.profileservice.dto.SinhVienDTO;
-import com.luanvan.profileservice.dto.request.CreateSinhVienRequest;
-import com.luanvan.profileservice.services.SinhVienService;
-import jakarta.inject.Inject;
-import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.mockito.Mockito.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ProfileServiceApplicationTests {
